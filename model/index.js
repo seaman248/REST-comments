2d3ccfaaf625fc9935af862a9e07deb948b38c66
seaman <semen.bko@ya.ru>
2015-01-26 18:14:11 +0300

    schema

---
+++ model/index.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/comments');
+
+var commentSchema = new mongoose.Schema({
+	author: {
+		type: String,
+		require: true,
+	},
+	text: {
+		type: String,
+		require: true,
+	},
+	date: {
+		type: Date,
+		require: true,
+		default: Date.now()
+	}
+})
