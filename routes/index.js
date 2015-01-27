ceeceeaeb01bffb2237e8ed3325ff435555dc299
seaman <semen.bko@ya.ru>
2015-01-27 16:46:48 +0300

    getapi

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 var Comment = require('../model').Comment;
-/* GET home page. */
+/* GET comments*/
 router.get('/', function(req, res) {
+	res.render('index');
+});
+
+router.get('/comments', function(req, res){
 	Comment.find(function(err, result){
+		res.json(result);
+	});
+})
+
+router.get('/comments/:id', function(req, res) {
+	console.log(req.body);
+	Comment.findOne({_id:req.params.id},function(err, result){
 		res.json(result);
 	});
 });
