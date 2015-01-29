cf47b2b0921251b1a9516d4fba8d76a4fbd557c7
seaman <semen.bko@ya.ru>
2015-01-29 23:06:11 +0300

    gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var react = require('gulp-react');
+
+
+gulp.task('react', function(){
+	gulp.src('./JScli/jsx/**/*.js')
+		.pipe(react())
+		.pipe(gulp.dest('./JScli/js'));
+});
+
+gulp.task('browserify', function(){
+	gulp.src('./JScli/js/**/*.js')
+		.pipe(browserify({
+			insertGlobals: true
+		}))
+		.pipe(gulp.dest('./public/js'))
+});
+
+gulp.task('watch',['react', 'browserify'] function(){
+	gulp.watch('./JScli/jsx/**/*.js', ['react' ,'browserify']);
+	gulp.watch('./JScli/js/**/*.js', ['browserify']);
+})
